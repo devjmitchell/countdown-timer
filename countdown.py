87abc9d31ecc8c39be3c5b2bdf418565da5361c3
semperphoenix <semperphoenixaz@gmail.com>
2017-08-04 09:32:59 -0700

    added local files

---
+++ countdown.py
@@ -0,0 +1,21 @@
+# import time
+
+# def countdown(t):
+#     while t:
+#         mins, secs = divmod(t, 60)
+#         timeformat = '{:02d}:{:02d}'.format(mins, secs)
+#         print(timeformat, end='\r')
+#         time.sleep(1)
+#         t -= 1
+#     print('Goodbye!\n')
+
+# countdown(120)
+
+import datetime
+
+fourpm = datetime.datetime.combine(
+    datetime.date.today(), 
+    datetime.time(17, 0))
+
+print(fourpm)
+
