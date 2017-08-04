cfcd1ed134f3beec00bec868825fc509f877b563
semperphoenix <semperphoenixaz@gmail.com>
2017-08-04 15:32:43 -0700

    playing around with datetime module

--- countdown.py
+++ countdown.py
@@ -11,11 +11,33 @@
 
 # countdown(120)
 
-import datetime
 
-fourpm = datetime.datetime.combine(
-    datetime.date.today(), 
-    datetime.time(17, 0))
+# import datetime
 
-print(fourpm)
+# end_time = datetime.datetime.combine(
+#     datetime.date.today(), 
+#     datetime.time(16, 0))
 
+# print(end_time)
+
+
+# import time
+# import datetime
+
+# format = "%H:%M"
+# timenow = time.strftime(format)
+# print("Current time: {}".format(timenow))
+# busleaves = input("What time does your bus leave? ")
+# bus = time.strptime(busleaves, format)
+# print("Bus leaves: {}".format(bus))
+# # timeleft = datetime.datetime(timenow, format)-datetime.datetime(bus, format)  #is this the right way to do the calculation???
+
+
+from datetime import datetime
+time_now = datetime.now().strftime("%H:%M")
+print(time_now)
+end_time = input("What is the end time? ")
+print(end_time)
+# delta = end_time - time_now
+# print(delta)
+# datetime.timedelta
